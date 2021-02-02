7353f75d34141bfd9b80ce1b8016ba572e3cc07c
1enaG <5555blackcat@gmail.com>
2021-02-02 20:26:56 +0200

    added something new

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,5 +3,5 @@
 @section("page-title", " Про проект ")
 
 @section("page-content")
-	<p>This is an app for keeping track of books in an e-library! :)</p>
+	<p>This is an app for keeping track of books in an e-library! SOMETHING NEW </p>
 @endsection
